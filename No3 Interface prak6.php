6d6fa0142ec7a0ebcd54bc160c6da291596e24e3
donnydamanik <89127709+donnydamanik@users.noreply.github.com>
2022-10-20 22:00:11 +0700

    Add files via upload

---
+++ No3 Interface prak6.php
@@ -0,0 +1,16 @@
+<!--- Nama    : AHMAD DONNY DAMANIK
+      NIM     : 21091397008
+      Kelas   : 2021 B--->
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
